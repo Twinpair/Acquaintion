9ad66551f64d0fcc19f50cd8065b5be501d4be2d
Eric Gonzalez <Twinpaireric@gmail.com>
2016-05-22 00:21:02 -0700

    Trivial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,8 @@
 module ApplicationHelper
+	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-    base_title = "Twitter Clone"
-    if page_title.empty?
-      base_title
-    else
-      base_title + " | " + page_title
-    end
-  end
+	    base_title = "Twitter Clone"
+	    base_title if page_title.empty?
+	    base_title + " | " + page_title
+	end
 end
