80cb9e1e45ab6ae965d1f7c0ea008f31731e41c0
Eric Gonzalez <Twinpaireric@gmail.com>
2016-08-21 00:40:20 -0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
+
 
   def show
-  	@user = User.find(params[:id])	
+    @user = User.find(params[:id])  
+  end
+
+  def new
+  	@user = User.new
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Signed Up Successfully!"
   		redirect_to @user
   	else
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+  test "should get new" do
+    get '/login'
+    assert_response :success
+  end
+
+end
