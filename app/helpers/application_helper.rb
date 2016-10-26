003ba896165a60725f017c19334efcda4200cb91
Eric Gonzalez <Twinpaireric@gmail.com>
2016-10-26 00:14:34 -0700

    Mobile Friendly Implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-	    base_title = "Twitter Clone"
+	    base_title = "Acquaintion"
 	    return base_title if page_title.empty?
 	    return base_title + " | " + page_title
 	end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Twitter Clone"
+    assert_select "title", "Acquaintion"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Twitter Clone | Help"
+    assert_select "title", "Acquaintion | Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "Twitter Clone | About"
+    assert_select "title", "Acquaintion | About"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Twitter Clone | Contact"
+    assert_select "title", "Acquaintion | Contact"
   end
 
 end
