6f81e672b8b07a9b3aa009d72ab53c27b1b14b28
Eric <twinpaireric@gmail.com>
2017-09-13 18:01:46 -0700

    Don't paginate post feeds

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
 	  if @micropost.save
 	    redirect_to root_url
 	  else
-	    @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
+	    @feed_items = current_user.feed
       render 'static_pages/home'
 	  end
   end
