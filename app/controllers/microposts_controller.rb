4bc2e94e6b7e3db400e50595fe9e4790254c749b
Eric <twinpaireric@gmail.com>
2017-09-13 17:37:50 -0700

    Load all feed items if micropost is not submitted successfully

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
 	  if @micropost.save
 	    redirect_to root_url
 	  else
-	    @feed_items = []
+	    @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
       render 'static_pages/home'
 	  end
   end
