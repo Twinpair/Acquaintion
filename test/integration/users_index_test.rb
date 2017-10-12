8d6726bda7218e3b9aaf194d1e1e3ca812ea18e7
Eric <twinpaireric@gmail.com>
2017-10-12 16:04:46 -0700

    Remove pagination assertion in tests

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -9,7 +9,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination'
     assert_select 'a', text: 'delete', count: 0
   end
 end
